636035029318181683f2d3a29b6cfffc9fcbde80
StanleyWang1 <swang11@stanford.edu>
2025-05-15 13:04:56 -0700

    tcp socket bug fix

--- CAMERA/tcp_server.py
+++ CAMERA/tcp_server.py
@@ -10,6 +10,7 @@ def main():
 
     # Setup socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((HOST, PORT))
     server_socket.listen(1)
     print(f"Server listening on {HOST}:{PORT}")
