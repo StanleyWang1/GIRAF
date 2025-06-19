f469063ebf78f12c4f1269cfaaa1a61e5625b839
StanleyWang1 <swang11@stanford.edu>
2025-06-19 15:41:22 -0700

    tuning traj3

--- CABLE_MANIP_CONTROL/cable_install_traj.py
+++ CABLE_MANIP_CONTROL/cable_install_traj.py
@@ -5,19 +5,19 @@ def generate_custom_trajectory():
     p1 = np.array([-0.2,  0.0,  -0.01])   # hold for 500 steps
     p2 = np.array([-0.2, -0.2, -0.01])
     p3 = np.array([ 0.2, -0.2, -0.01])
-    p4 = np.array([ 0.2,  0.0,  -0.01])
-    p5 = np.array([ 0.0,  0.0,  -0.01])
-    p6 = np.array([ 0.0,  0.0,  -0.2])
-    p7 = np.array([ 0.0, -0.2,  -0.2])
+    p4 = np.array([ 0.2,  0.1,  -0.01])
+    p5 = np.array([ 0.0,  0.1,  -0.01])
+    p6 = np.array([ 0.0,  0.1,  -0.1])
+    p7 = np.array([ 0.0, -0.4,  -0.1])
 
     # Build trajectory
-    traj1 = np.tile(p1, (500, 1))
-    traj2 = np.tile(p2, (500, 1))
-    traj3 = np.tile(p3, (500, 1))
-    traj4 = np.tile(p4, (500, 1))
-    traj5 = np.tile(p5, (500, 1))
-    traj6 = np.tile(p6, (500, 1))
-    traj7 = np.tile(p7, (500, 1))
+    traj1 = np.tile(p1, (300, 1))
+    traj2 = np.tile(p2, (300, 1))
+    traj3 = np.tile(p3, (300, 1))
+    traj4 = np.tile(p4, (300, 1))
+    traj5 = np.tile(p5, (300, 1))
+    traj6 = np.tile(p6, (300, 1))
+    traj7 = np.tile(p7, (300, 1))
 
     trajectory = np.vstack([traj1, traj2, traj3, traj4, traj5, traj6, traj7])
     return trajectory
