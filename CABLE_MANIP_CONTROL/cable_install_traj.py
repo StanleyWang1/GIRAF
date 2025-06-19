b6dfd82e9d799a33997e13c4adb0df89c7104922
StanleyWang1 <swang11@stanford.edu>
2025-06-19 15:35:00 -0700

    traj3

--- CABLE_MANIP_CONTROL/cable_install_traj.py
+++ CABLE_MANIP_CONTROL/cable_install_traj.py
@@ -2,13 +2,13 @@ import numpy as np
 
 def generate_custom_trajectory():
     # Define points
-    p1 = np.array([-0.1,  0.0,  -0.02])   # hold for 500 steps
-    p2 = np.array([-0.1, -0.15, -0.02])
-    p3 = np.array([ 0.1, -0.15, -0.02])
-    p4 = np.array([ 0.1,  0.0,  -0.02])
-    p5 = np.array([ 0.0,  0.0,  -0.02])
-    p6 = np.array([ 0.0,  0.0,  -0.15])
-    p7 = np.array([ 0.0, -0.2,  -0.15])
+    p1 = np.array([-0.2,  0.0,  -0.01])   # hold for 500 steps
+    p2 = np.array([-0.2, -0.2, -0.01])
+    p3 = np.array([ 0.2, -0.2, -0.01])
+    p4 = np.array([ 0.2,  0.0,  -0.01])
+    p5 = np.array([ 0.0,  0.0,  -0.01])
+    p6 = np.array([ 0.0,  0.0,  -0.2])
+    p7 = np.array([ 0.0, -0.2,  -0.2])
 
     # Build trajectory
     traj1 = np.tile(p1, (500, 1))
