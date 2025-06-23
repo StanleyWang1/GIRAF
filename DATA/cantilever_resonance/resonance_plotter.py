cbee60f0c32b1ae1aabf7126908a733944158ffb
StanleyWang1 <swang11@stanford.edu>
2025-06-23 16:40:34 -0700

    100cm tests

--- DATA/cantilever_resonance/resonance_plotter.py
+++ DATA/cantilever_resonance/resonance_plotter.py
@@ -2,15 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load CSV
-df = pd.read_csv("./DATA/cantilever_resonance/log1.csv")
-
-# Plot pitch over time
-plt.figure()
-plt.plot(df['t'], df['pitch'])
-plt.xlabel("Time (s)")
-plt.ylabel("Pitch (ticks or rad)")
-plt.title("Pitch vs Time")
-plt.grid(True)
+df = pd.read_csv("./DATA/cantilever_resonance/boom_50cm_trial3.csv")
 
 # Plot accelerometer axes
 plt.figure()
