3e42091cd5fd9a5a2970cded766d5badee182cd0
StanleyWang1 <swang11@stanford.edu>
2025-06-29 17:30:19 -0700

    update

--- CABLE_MANIP_CONTROL/new_cable_traj.py
+++ CABLE_MANIP_CONTROL/new_cable_traj.py
@@ -7,7 +7,7 @@ def generate_custom_trajectory():
     p1 = np.array([-0.25, -0.05, -0.1])
     p2 = np.array([-0.1, -0.05, -0.085])
     p3 = np.array([-0.1,  0.05, -0.085])
-    p4 = np.array([-0.15,  0.02, -0.1])
+    p4 = np.array([-0.175,  0.02, -0.1])
     p5 = np.array([-0.25, -0.1, -0.2])
     p6 = np.array([-0.15, -0.1, -0.1])
     # 400-point constant segment
