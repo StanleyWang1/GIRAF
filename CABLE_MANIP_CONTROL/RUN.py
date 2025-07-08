02972fe2c2ea5769a52382d76e7387be9d391c4f
StanleyWang1 <swang11@stanford.edu>
2025-07-08 14:44:09 -0700

    removed sleep

--- CABLE_MANIP_CONTROL/RUN.py
+++ CABLE_MANIP_CONTROL/RUN.py
@@ -247,7 +247,7 @@ def motor_control():
                                                       radians_to_ticks(theta5_pos) + MOTOR12_HOME,
                                                       radians_to_ticks(theta6_pos) + MOTOR13_HOME,
                                                       gripper_pos])
-            time.sleep(0.005)
+            # time.sleep(0.005)
             loop_end = time.time()    # <--- END TIMESTAMP
             print(f"[motor_control] Loop time: {(loop_end - loop_start)*1000:.2f} ms")
     
