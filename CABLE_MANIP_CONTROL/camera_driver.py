d63134b3e17aba1171babf3fae0fb5678a256ed9
StanleyWang1 <swang11@stanford.edu>
2025-07-09 15:20:11 -0700

    syntax fix

--- CABLE_MANIP_CONTROL/camera_driver.py
+++ CABLE_MANIP_CONTROL/camera_driver.py
@@ -7,8 +7,8 @@ import time
 def get_camera_intrinsics(device):
     calib = device.readCalibration()
     # intrinsics = np.array(calib.getCameraIntrinsics(dai.CameraBoardSocket.RGB, 640, 480))
-    intrinsics = np.array([[704.584, 0.0,     325.885]
-                            [0.0,    704.761, 245.785]
+    intrinsics = np.array([[704.584, 0.0,     325.885],
+                            [0.0,    704.761, 245.785],
                             [0.0,    0.0,     1.0]])
     return intrinsics
 
