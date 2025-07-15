bed796aee8613435097bbdfa1e89d8b0ee2cc207
StanleyWang1 <swang11@stanford.edu>
2025-07-15 14:29:07 -0700

    square

--- CABLE_MANIP_CONTROL/square_traj.py
+++ CABLE_MANIP_CONTROL/square_traj.py
@@ -1,21 +1,21 @@
 import numpy as np
 
 def generate_square_xy_trajectory():
-    # z = -0.2  # Constant height
+    z = -0.2  # Constant height
 
     # Define square corners in clockwise or counterclockwise order
-    # p1 = np.array([-0.25,  -0.05, z])
-    # p2 = np.array([-0.15,  -0.05, z])
-    # p3 = np.array([-0.15,   0.05, z])
-    # p4 = np.array([-0.25,   0.05, z])
+    p1 = np.array([-0.25,  -0.05, z])
+    p2 = np.array([-0.15,  -0.05, z])
+    p3 = np.array([-0.15,   0.05, z])
+    p4 = np.array([-0.25,   0.05, z])
     # z = 0.25  # Constant height
 
-    z = 0.25
-    # # Define square corners in clockwise or counterclockwise order
-    p1 = np.array([0.5,  -0.25, z])
-    p2 = np.array([1.0,  -0.25, z])
-    p3 = np.array([1.0,   0.25, z])
-    p4 = np.array([0.5,   0.25, z])
+    # z = 0.25
+    # # # Define square corners in clockwise or counterclockwise order
+    # p1 = np.array([0.5,  -0.25, z])
+    # p2 = np.array([1.0,  -0.25, z])
+    # p3 = np.array([1.0,   0.25, z])
+    # p4 = np.array([0.5,   0.25, z])
 
     # Create linear segments
     traj1 = np.linspace(p1, p2, 200)
