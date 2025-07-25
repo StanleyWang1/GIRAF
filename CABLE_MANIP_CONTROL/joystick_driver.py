45939be1005c98fa545cdd10021a7cefd245c734
StanleyWang1 <swang11@stanford.edu>
2025-07-25 14:29:08 -0700

    remap

--- CABLE_MANIP_CONTROL/joystick_driver.py
+++ CABLE_MANIP_CONTROL/joystick_driver.py
@@ -29,10 +29,10 @@ def joystick_read(js):
         "YB": js.get_button(3),
         "LB": js.get_button(4),
         "RB": js.get_button(5),
-        "DPADUP": js.get_button(11),  # replace with your button numbers
-        "DPADDOWN": js.get_button(12),
-        "DPADLEFT": js.get_button(13),
-        "DPADRIGHT": js.get_button(14)
+        "DPADUP": js.get_button(6),  # replace with your button numbers
+        "DPADDOWN": js.get_button(7),
+        "DPADLEFT": js.get_button(8),
+        "DPADRIGHT": js.get_button(9)
     }
 
 def joystick_disconnect(js):
